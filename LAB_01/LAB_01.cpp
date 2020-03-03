eef7838fdae15519f35bf41bbb56a31438df1b88
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-03-03 07:53:01 +0100

    Creates task one and two function placeholders;

--- LAB_01/LAB_01.cpp
+++ LAB_01/LAB_01.cpp
@@ -6,7 +6,19 @@ const float C = 3;
 const float D = 4;
 const float E = 4;
 
+
+void taskOne(float startT, float endT, float deltaT)
+{
+
+}
+
+void taskTwo(float startT, float endT, float deltaT)
+{
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	taskOne(-10, 10, 1 / 100);
+	taskTwo(-1, 1, 1 / 22050);
 }
