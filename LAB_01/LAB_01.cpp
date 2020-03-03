c950037678a327354d2baed778a3c6310dfd684a
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-03-03 08:52:57 +0100

    Changes float to double to increase precision;

--- LAB_01/LAB_01.cpp
+++ LAB_01/LAB_01.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-const float A = 7;
-const float B = 6;
-const float C = 3;
-const float D = 4;
-const float E = 4;
+const double A = 7;
+const double B = 6;
+const double C = 3;
+const double D = 4;
+const double E = 4;
 
-float x(float t)
+double x(double t)
 {
 	return A * t * t + B * t + C;
 }
 
-void solveSquareEquation(float a, float b, float c)
+void solveSquareEquation(double a, double b, double c)
 {
 	auto discriminant = b * b - 4 * a * c;
 	if (discriminant < 0)
@@ -23,17 +24,17 @@ void solveSquareEquation(float a, float b, float c)
 		std::cout << "There are two roots. x1 = " << ((-b - sqrt(discriminant)) / 2 * a) << " x2 = " << ((-b + sqrt(discriminant)) / 2 * a);
 }
 
-std::vector<float> solveFunction(std::vector<float> arguments, float (*function)(float))
+std::vector<double> solveFunction(std::vector<double> arguments, double (*function)(double))
 {
-	std::vector<float> results;
+	std::vector<double> results;
 	for (auto argument : arguments)
 		results.push_back(function(argument));
 	return results;
 }
 
-std::vector<float> fillVector(float startT, float endT, float deltaT)
+std::vector<double> fillVector(double startT, double endT, double deltaT)
 {
-	std::vector<float> range;
+	std::vector<double> range;
 	auto t = startT;
 	while (t <= endT)
 	{
@@ -43,23 +44,23 @@ std::vector<float> fillVector(float startT, float endT, float deltaT)
 	return range;
 }
 
-void saveResult(std::vector<float> arguments, std::vector<float> results, std::string filename)
+void saveResult(std::vector<double> arguments, std::vector<double> results, std::string filename)
 {
 
 }
 
-void taskOne(float startT, float endT, float deltaT)
+void taskOne(double startT, double endT, double deltaT)
 {
 	solveSquareEquation(A, B, C);
 }
 
-void taskTwo(float startT, float endT, float deltaT)
+void taskTwo(double startT, double endT, double deltaT)
 {
 
 }
 
 int main()
 {
-	taskOne(-10, 10, 1 / 100);
-	taskTwo(-1, 1, 1 / 22050);
+	taskOne(-10, 10, 1.0 / 100.0);
+	taskTwo(-1, 1, 1,0 / 22050.0);
 }
