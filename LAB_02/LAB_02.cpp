95232df12404362a325e7275b7f5be7656e33196
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-03-09 12:53:42 +0100

    Adds fillVector function;

--- LAB_02/LAB_02.cpp
+++ LAB_02/LAB_02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #define PI 3.14159265358979323846
 
@@ -15,6 +16,19 @@ double s(double t)
     const double PHI = C * PI;
     return A * sin(2 * PI * F * t + PHI);
 }
+
+std::vector<double> fillVector(double startT, double endT, double deltaT)
+{
+	std::vector<double> range;
+	auto t = startT;
+	while (t <= endT)
+	{
+		range.push_back(t);
+		t += deltaT;
+	}
+	return range;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
