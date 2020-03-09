b3ea33917ff9f1f4e401c4ca8ee617cf17ca223d
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-03-09 13:20:42 +0100

    Updates charts, so they show points instead of lines + add decimation;

--- LAB_01/chartDrawing.py
+++ LAB_01/chartDrawing.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 functionNames = ('x', 'y', 'z', 'u', 'v', 'p2', 'p4', 'pAB')
+decimation = 5
 
 def drawPlot(functionName):
     t, y = np.loadtxt('Outputs/' + functionName + '.csv', delimiter=',', unpack=True, skiprows = 1)
     plt.clf()
-    plt.plot(t, y, color='tab:blue')
+
+    plt.scatter(t[::decimation], y[::decimation], 1, color='tab:blue', )
     plt.title('Plot of ' + functionName + '(t) function')
     plt.xlabel('t')
-    plt.ylabel('y')
+    plt.ylabel(functionName + '(t)')
     plt.grid(axis='y')
     plt.savefig('Charts/' + functionName + '.png', dpi=200)
 
