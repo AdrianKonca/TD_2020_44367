41b621c29fa0ee2dacec3d197857902d036b382c
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-03-09 12:30:09 +0100

    Defines required constants;

--- LAB_02/LAB_02.cpp
+++ LAB_02/LAB_02.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#define PI 3.14159265358979323846
+
+const double A = 7;
+const double B = 6;
+const double C = 3;
+const double D = 4;
+const double E = 4;
+
 int main()
 {
     std::cout << "Hello World!\n";
