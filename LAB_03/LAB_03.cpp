f90c3ba560d898b955a309fcb91f431f4a8c4d7a
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-03-23 23:41:19 +0100

    Adds fourth task solution and refactor;

--- LAB_03/LAB_03.cpp
+++ LAB_03/LAB_03.cpp
@@ -15,6 +15,12 @@ const double AMPLITUDE = 1.0;
 const double FREQUENCY = B;
 const double PHASE_ANGLE = C * PI;
 
+struct FileRow
+{
+	double t;
+	double y;
+};
+
 double s(double t)
 {
 	return AMPLITUDE * sin(2 * PI * FREQUENCY * t + PHASE_ANGLE);
@@ -75,6 +81,12 @@ std::vector<double> frequencyScaleCalculator(double samplingFrequency, int sampl
 	return frequencies;
 }
 
+std::vector<double> getFirstHalfOfVector(std::vector<double> vectorToSplit)
+{
+	std::vector<double> firstHalf(vectorToSplit.begin(), vectorToSplit.begin() + vectorToSplit.size() / 2);
+	return firstHalf;
+}
+
 std::vector <double> invertedDiscreteFourierTransformation(std::vector<std::complex<double>> transformedSamples)
 {
 	int SAMPLES_COUNT = transformedSamples.size();
@@ -94,25 +106,105 @@ std::vector<double> amplitudeSpectrumTransformation(std::vector<std::complex<dou
 {
 	std::vector<double> amplitudes(transformedSamples.size());
 	for (auto i = 0; i < transformedSamples.size(); ++i)
-		amplitudes[i] = abs(transformedSamples[i]);
+		amplitudes[i] = abs(transformedSamples[i]) / transformedSamples.size() * 2;
 	return amplitudes;
 }
 
-void secondTask()
+std::vector<FileRow> readInputFile(std::string filename)
 {
-	const double STARTING_T = 0.0;
-	const double SAMPLING_FREQENCY = 1000.0;
-	const double DELTA_T = 1.0 / SAMPLING_FREQENCY;
-	const double FINISHING_T = A * 100 + B * 10 + C;
+
+	std::ifstream dataFile("Inputs/" + filename);
+	std::vector<FileRow> fileRows;
+
+	double t, y;
+	char delimiter;
+	
+	//ignore first line
+	dataFile.ignore(1000, '\n');
+
+	while (dataFile >> t >> delimiter >> y)
+		fileRows.push_back({t, y});
+	
+	return fileRows;
 }
 
+std::vector<FileRow> getEveryNthSample(std::vector<FileRow> values, int samplesCount)
+{
+	auto step = (double)values.size() / (double)samplesCount;
+	double currentStep = 0;
+	std::vector<FileRow> nthValues;
+	for (auto i = 0; i < samplesCount; i++)
+	{
+ 		nthValues.push_back(values[round(currentStep)]);
+		currentStep += step;
+	}
+	return nthValues;
+}
+
+std::vector<double> getTValuesColumn(std::vector<FileRow> values)
+{
+	std::vector<double> tValues(values.size());
+	for (auto i = 0; i < values.size(); i++)
+		tValues[i] = values[i].t;
+	return tValues;
+}
+
+std::vector<double> getYValuesColumn(std::vector<FileRow> values)
+{
+	std::vector<double> yValues(values.size());
+	for (auto i = 0; i < values.size(); i++)
+		yValues[i] = values[i].y;
+	return yValues;
+}
+
+void secondAndFourthTask()
+{
+	const double STARTING_T = 0.0;
+	const double SAMPLING_FREQENCY = 1000;
+	const double DELTA_T = 1.0 / SAMPLING_FREQENCY;
+	const double SAMPLE_COUNT = A * 100 + B * 10 + C;
+	const double FINISHING_T = SAMPLE_COUNT * DELTA_T;
+
+	auto tValues = fillVector(STARTING_T, FINISHING_T, DELTA_T);
+	auto sValues = solveFunction(tValues, s);
+	auto sTransformed = discreteFourierTransformation(sValues);
+	auto sAmplitudes = getFirstHalfOfVector(amplitudeSpectrumTransformation(sTransformed));
+	auto frequencyScale = getFirstHalfOfVector(frequencyScaleCalculator(SAMPLING_FREQENCY, sTransformed.size()));
+	auto sValuesReversed = invertedDiscreteFourierTransformation(sTransformed);
+	saveResult<double, double>(tValues, sValues, "s.csv");
+	saveResult<double, double>(frequencyScale, sAmplitudes, "sAmplitudes.csv");
+	saveResult<double, double>(tValues, invertedDiscreteFourierTransformation(sTransformed), "sReversed.csv");
+	saveResult<double, double>(tValues, sValuesReversed, "sReversed.csv");
+}
+
+void thirdSubTask(std::string filename)
+{
+	const double SAMPLE_COUNT = A * 100 + B * 10 + C;
+	const double SAMPLING_FREQENCY = 22050 / SAMPLE_COUNT;
+	auto values = getEveryNthSample(readInputFile(filename + ".csv"), SAMPLE_COUNT);
+	auto tValues = getTValuesColumn(values);
+	auto yValues = getYValuesColumn(values);
+	
+	auto yTransformed = discreteFourierTransformation(yValues);
+	auto yAmplitudes = getFirstHalfOfVector(amplitudeSpectrumTransformation(yTransformed));
+	auto frequencyScale = getFirstHalfOfVector(frequencyScaleCalculator(SAMPLING_FREQENCY, yTransformed.size()));
+	saveResult<double, double>(tValues, yValues, filename + ".csv");
+	saveResult<double, double>(frequencyScale, yAmplitudes, filename + "Amplitudes.csv");
+}
 void thirdTask()
 {
-
+	thirdSubTask("p2");
+	thirdSubTask("p4");
+	thirdSubTask("pAB");
+	thirdSubTask("x");
+	thirdSubTask("y");
+	thirdSubTask("v");
+	thirdSubTask("z");
+	thirdSubTask("u");
 }
 
 int main()
 {
-	secondTask();
+	secondAndFourthTask();
 	thirdTask();
 }
