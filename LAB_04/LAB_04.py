67d96d84e88968efed2b0c48a600f23c48385f10
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-04-06 20:02:38 +0200

    Fixes initial version after comments from instructor;

--- LAB_04/LAB_04.py
+++ LAB_04/LAB_04.py
@@ -8,18 +8,22 @@ D = 4
 E = 4
 
 AMPLITUDE = 1.0
-FREQUENCY = 1
+FREQUENCY = 4
 
 #Ka Modulation depth for AM
 #Kp Modulation depth for PM
 def informationSignal(t):
     return AMPLITUDE * np.sin(2 * np.pi * FREQUENCY * t)
 
+def carryingSignal(t, modulationFrequency):
+    return np.cos(2 * np.pi * modulationFrequency * t)
+
 def amplitudeModulation(samples, modulationDepth, modulationFrequency, t):
-    return (modulationDepth + (samples + AMPLITUDE)) * np.cos(2 * np.pi * modulationFrequency * t)
+    carryingSignalSamples = carryingSignal(t, modulationFrequency)
+    return carryingSignalSamples, (modulationDepth + (samples + AMPLITUDE)) * carryingSignalSamples
 
 def phaseModulation(samples, modulationDepth, modulationFrequency, t):
-    return np.cos((2 * np.pi * modulationFrequency * t) + (modulationDepth * samples))
+    return carryingSignal(t, modulationFrequency), np.cos((2 * np.pi * modulationFrequency * t) + np.radians(modulationDepth * samples))
 
 def filterNoise(filterParameter, amplitudes, frequencies):
     frequenciesFiltered = []
@@ -34,7 +38,7 @@ def filterNoise(filterParameter, amplitudes, frequencies):
 def frequencyScaleCalculator(samplingFrequency, samplesCount):
 	frequencies = []
 	for i in range(samplesCount):
-		frequencies.append(i * samplingFrequency / samplesCount)
+		frequencies.append(i * samplingFrequency / samplesCount / 2)
 	return np.array(frequencies)
 
 def convertToDecibelScale(amplitudes):
@@ -43,35 +47,44 @@ def convertToDecibelScale(amplitudes):
 def convertFftToAmplitudeSpectrum(fftValues):
     return abs(np.array(fftValues))/len(fftValues)
 
-def taskOne(amplitudeModulationDepth, phaseModulationDepth):
-    SAMPLING_FREQUENCY = 10000
+def drawModulation(t, modulationSamples, carryingSamples, modulationLabel, subplot):
+    plt.subplot(5, 1, subplot)
+    plt.scatter(t, carryingSamples, 1, color='tab:red', label = 'Carrier signal')
+    plt.scatter(t, modulationSamples, 1, color='tab:blue', alpha=0.1, label = modulationLabel)
+    plt.title("Amplitude modulation")
+    plt.xlabel("Time")
+    plt.ylabel("Signal strength")
+    plt.legend()
 
-    t = np.linspace(0, 1, SAMPLING_FREQUENCY)
+def drawAmplitudeSpectrum(frequencies, amplitudeSamples, title, subplot):
+    plt.subplot(5, 1, subplot)
+    plt.stem(frequencies, convertToDecibelScale(amplitudeSamples), markerfmt = 'C1o', use_line_collection=True)
+    plt.title(title)
+    plt.ylabel('Decibels')
+    plt.xlabel('Frequency')
+
+
+def taskOne(amplitudeModulationDepth, phaseModulationDepth, filename):
+    SAMPLING_FREQUENCY = 25000
+    TIME = 1
+    SAMPLES_COUNT = TIME * SAMPLING_FREQUENCY
+    t = np.linspace(0, TIME, SAMPLES_COUNT)
 
     samples = informationSignal(t)
-    samplesAfterAmplitudeModulation = amplitudeModulation(samples, amplitudeModulationDepth, 100, t)
-    samplesAfterPhaseModulation = phaseModulation(samples, phaseModulationDepth, 25, t)
-
+    amplitudeCarryingSamples, amplitudeModulationSamples = amplitudeModulation(samples, amplitudeModulationDepth, 100, t)
+    phaseCarryingSamples, phaseModulationSamples = phaseModulation(samples, phaseModulationDepth, 40, t)
+    
     plt.figure(figsize = (16, 9))
-    #plt.figure()
     plt.subplot(5, 1, 1)
     plt.scatter(t, samples, 1, color='tab:blue')
     plt.title("Pure tone")
     plt.xlabel("Time")
     plt.ylabel("Signal strength")
-    plt.subplot(5, 1, 2)
-    plt.scatter(t, samplesAfterAmplitudeModulation, 1, color='tab:blue')
-    plt.title("Amplitude modulation")
-    plt.xlabel("Time")
-    plt.ylabel("Signal strength")
-    plt.subplot(5, 1, 3)
-    plt.scatter(t, samplesAfterPhaseModulation, 1, color='tab:blue')
-    plt.title("Phase modulation")
-    plt.xlabel("Time")
-    plt.ylabel("Signal strength")
+    drawModulation(t, amplitudeModulationSamples, amplitudeCarryingSamples, 'AM signal', 2)
+    drawModulation(t, phaseModulationSamples, phaseCarryingSamples, 'PM signal', 4)
 
-    fftAmplitude = np.fft.rfft(samplesAfterAmplitudeModulation)
-    fftPhase = np.fft.rfft(samplesAfterPhaseModulation)
+    fftAmplitude = np.fft.rfft(amplitudeModulationSamples)
+    fftPhase = np.fft.rfft(phaseModulationSamples)
 
     frequency = frequencyScaleCalculator(SAMPLING_FREQUENCY, len(fftAmplitude))
 
@@ -81,23 +94,31 @@ def taskOne(amplitudeModulationDepth, phaseModulationDepth):
     phaseSpectrum = convertFftToAmplitudeSpectrum(fftPhase)
     phaseFrequencies, phaseFilteredAmplitudeSpectrum = filterNoise(500, phaseSpectrum, frequency)
 
-    plt.subplot(5, 1, 4)
-    plt.stem(amplitudeFrequencies, convertToDecibelScale(amplitudeFilteredAmplitudeSpectrum), markerfmt = 'C1o', use_line_collection=True)
-    plt.title("Amplitude spectrum of AM")
-    plt.ylabel('Decibels')
-    plt.xlabel('Frequency')
-    plt.subplot(5, 1, 5)
-    plt.stem(phaseFrequencies, convertToDecibelScale(phaseFilteredAmplitudeSpectrum), markerfmt = 'C1o', use_line_collection=True)
-    plt.title("Amplitude spectrum of PM")
-    plt.ylabel('Decibels')
-    plt.xlabel('Frequency')
+    drawAmplitudeSpectrum(amplitudeFrequencies, amplitudeFilteredAmplitudeSpectrum, 'Amplitude spectrum of AM', 3)
+    drawAmplitudeSpectrum(phaseFrequencies, phaseFilteredAmplitudeSpectrum, 'Amplitude spectrum of PM', 5)
     plt.tight_layout()
+    plt.savefig('Charts/' + filename, dpi = 150)
 
 
-
-
-
-taskOne(0.5, 1)
-taskOne(10, 3.14)
-taskOne(100, 100)
+taskOne(0.5, 1, "modulationA.png")
+taskOne(10, 3.14, "modulationB.png")
+taskOne(100, 90, "modulationC.png")
 plt.show()
+
+#Niebieski sygnał AM i PM rysowany jest z lekką alfą - ponieważ Carry Signal PM był bardzo podobny do PM.
+#Ponadto, alfa ta pozwala zauważyć zagęszczenia w wartościach PM - zwłaszcza na wykresie C
+
+#Zadanie 3
+#A
+#   fmin AM = 96Hz, fmax AM = 104Hz, W = 8Hz
+#   fmin PM = 40Hz, fmax PM = 40Hz, W = 0Hz
+#B
+#   fmin AM = 96Hz, fmax AM = 104Hz, W = 8Hz
+#   fmin PM = 40Hz, fmax PM = 40Hz, W = 0Hz
+#C
+#   fmin AM = 96Hz, fmax AM = 104Hz, W = 8Hz
+#   fmin PM = 36Hz, fmax PM = 44Hz, W = 8Hz
+
+#   Wydaje mi się, że im mniejsze W (szerokość pasma, bandwidth) tym lepiej dla sygnału radiowego, ponieważ zmniejsza on
+#   ilość potrzebnych do obserwacji pasm oraz pozwala na puszczanie wielu sygnałów naraz (ale tak naprawde nie wiem czy 
+#   jest to prawda, tak tylko mi się wydaje)
